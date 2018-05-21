5c9a3f7350c02f60c51b103f1ce8a8b625594208
cmontella <cmontella@live.com>
2018-05-20 20:40:58 -0700

    Add instances

--- libraries/html/html.ts
+++ libraries/html/html.ts
@@ -2,11 +2,44 @@ import {Program, Library, createId, RawValue, RawEAV, RawMap, handleTuples} from
 
 const EMPTY:never[] = [];
 
+export interface Instance extends HTMLElement {
+  /*
+  __element: RawValue,
+  __source: HTML,
+  __styles?: RawValue[],
+  __sort?:RawValue,
+  __autoSort?:RawValue,
+  __listeners?: {[event:string]: boolean},
+  __capturedKeys?: {[code:number]: boolean}
+  */
+}
+
 export class HTML extends Library {
   static id = "html";
 
+  /** Instances are the physical DOM elements representing table elements. */
+  _instances: number[][] = [];
+
   setup() {
     console.log("HELLO HTML!!!!!");
+  }
+
+  protected addInstance(table: number, row: number, column: number, value: number) {
+    row = row - 1;
+    column = column - 1; 
+    //if(id === null || id === "null") throw new Error(`Cannot create instance with null id for element '${elemId}'.`);
+    if (this._instances[row] === undefined) {
+      this._instances[row] = [];
+    }
+    this._instances[row][column] = value;
+    console.log(this._instances);
+    //if(instance) throw new Error(`Recreating existing instance '${id}'`);
+    //if(ns) instance = this.decorate(document.createElementNS(""+ns, ""+tagname), elemId);
+    //else instance = this.decorate(document.createElement(""+tagname), elemId);
+    //if(!this._elementToInstances[elemId]) this._elementToInstances[elemId] = [id];
+    //else this._elementToInstances[elemId].push(id);
+    //return this._instances[id] = instance;
+    
   }
 
   handlers = {
@@ -15,9 +48,9 @@ export class HTML extends Library {
         console.log(remove);
         //this.removeInstance(instanceId);
       }
-      for(let [handler, tag, row, column, value] of adds || EMPTY) {
-        console.log(handler, tag, row, column, value);
-        //this.addInstance(instanceId, elemId, tagname, ns);
+      for(let [handler, table, row, column, value] of adds || EMPTY) {
+        console.log(handler, table, row, column, value);
+        this.addInstance(table, row, column, value);
       }
     })
   };
