f98a60b61ef92d4d8920e81dc45bf9796dfedaf2
cmontella <cmontella@live.com>
2018-08-13 22:28:41 -0400

    put spaces around colons

--- src/library.ts
+++ src/library.ts
@@ -7,33 +7,33 @@
 export type RawValue = number|string;
 export type RawTuple = number[];
 export type RawChange = [number, number, number, RawValue];
-export interface RawMap<V> {[key:string]: V, [key:number]: V};
+export interface RawMap<V> {[key: string]:  V, [key: number]:  V};
 export type RawRecord = RawMap<RawValue|RawValue[]>;
 
 // Diffs
 
-export type Diff<T> = {adds?: T, removes?: T};
-export type DiffHandler = (diff:Diff<RawTuple[]>) => void
-export type EAVDiffHandler = (diff:Diff<RawChange[]>) => void
-export type RecordDiffHandler<T extends RawRecord> = (diff:Diff<RawMap<T>>) => void
+export type Diff<T> = {adds?:  T, removes?:  T};
+export type DiffHandler = (diff: Diff<RawTuple[]>) => void
+export type EAVDiffHandler = (diff: Diff<RawChange[]>) => void
+export type RecordDiffHandler<T extends RawRecord> = (diff: Diff<RawMap<T>>) => void
 
 // ## Program
 
 export interface Program {
-  name:string;
-  libraries:{[id:string]: Library};
+  name: string;
+  libraries: {[id: string]:  Library};
 
-  send_transaction(transaction:RawChange[]):this;
-  attach(libraryId:string):Library;
-  attached(libraryId:string, library:Library):void;
+  send_transaction(transaction: RawChange[]): this;
+  attach(libraryId: string): Library;
+  attached(libraryId: string, library: Library): void;
 }
 
 // ## Library
 
 export class Library {
-  protected static _registry:{[id:string]: typeof Library} = {};
+  protected static _registry: {[id: string]:  typeof Library} = {};
 
-  static register(id:string, library:typeof Library) {
+  static register(id: string, library: typeof Library) {
     if(this._registry[id]) {
       if(this._registry[id] === library) return;
       throw new Error(`Attempting to overwrite existing library with id '${id}'`);
@@ -41,30 +41,30 @@ export class Library {
     this._registry[id] = library;
   }
 
-  static unregister(id:string) {
+  static unregister(id: string) {
     delete this._registry[id];
   }
 
-  static get(id:string) {
+  static get(id: string) {
     let library = this._registry[id];
     if(library) return library;
   }
 
-  static attach(program:Program, libraryId:string):Library {
+  static attach(program: Program, libraryId: string): Library {
     let LibraryCtor = Library.get(libraryId);
     if(!LibraryCtor) throw new Error(`Unable to attach unknown library '${libraryId}'.`);
     if(program.libraries[libraryId]) return program.libraries[libraryId];
-    let library:Library = new LibraryCtor(program);
+    let library: Library = new LibraryCtor(program);
     program.libraries[libraryId] = library;
     library.setup();
     program.attached(libraryId, library);
     return library;
   }
 
-  static id:string;
-  handlers:{[name:string]: DiffHandler};
+  static id: string;
+  handlers: {[name: string]:  DiffHandler};
 
-  protected _order: string[]|undefined;
+  protected _order:  string[]|undefined;
 
   get order() {
     if(this._order) return this._order;
@@ -72,7 +72,7 @@ export class Library {
   }
   get program() { return this._program; }
 
-  constructor(protected _program:Program) {}
+  constructor(protected _program: Program) {}
 
   setup() {}
 }
@@ -80,24 +80,24 @@ export class Library {
 // ## Handlers
 
 // Just a convenience fn for type hinting.
-export function handleTuples(handler:DiffHandler): DiffHandler {
+export function handleTuples(handler: DiffHandler):  DiffHandler {
   return handler;
 }
 
-export function handleEAVs(handler:EAVDiffHandler): DiffHandler {
+export function handleEAVs(handler: EAVDiffHandler):  DiffHandler {
   return (diffs) => {
     let sample = (diffs.adds && diffs.adds[0]) || (diffs.removes && diffs.removes[0]);
     if(sample) {
       if(sample.length < 3) throw new Error(`Unable to parse EAV from tuple with < 3 fields. ${JSON.stringify(sample)}`);
-      if(sample.length > 3) console.warn(`Expected 3 values to parse EAV from tuple, got: ${sample.length}`);
+      if(sample.length > 3) console.warn(`Expected 3 values to parse EAV from tuple, got:  ${sample.length}`);
     }
     handler(diffs as Diff<RawChange[]>);
   };
 }
 
-export function handleRecords<T extends RawRecord>(attributes: string[], handler:RecordDiffHandler<T>): DiffHandler {
+export function handleRecords<T extends RawRecord>(attributes:  string[], handler: RecordDiffHandler<T>):  DiffHandler {
   return (diffs) => {
-    let recordDiffs:Diff<RawMap<T>> = {adds: {}, removes: {}};
+    let recordDiffs: Diff<RawMap<T>> = {adds:  {}, removes:  {}};
     if(diffs.adds) {
       for(let add of diffs.adds) {
         let id = add[0];
@@ -118,7 +118,7 @@ export function handleRecords<T extends RawRecord>(attributes: string[], handler
 
 // ## Helpers
 
-export function asValue(value:RawValue|undefined) {
+export function asValue(value: RawValue|undefined) {
   if(typeof value == "string") {
     if(value == "true") return true;
     if(value == "false") return false;
@@ -130,7 +130,7 @@ export function createId() {
   return "|";
 }
 
-export function tupleToRecord<T extends RawRecord>(attributes: string[], tuple:RawTuple, record:RawRecord = {}): T {
+export function tupleToRecord<T extends RawRecord>(attributes:  string[], tuple: RawTuple, record: RawRecord = {}):  T {
   let ix = 1; // First slot is ID
   let asArrays = false;
   for(let attr of attributes) {
@@ -152,10 +152,10 @@ export function tupleToRecord<T extends RawRecord>(attributes: string[], tuple:R
   return record as T;
 }
 
-export function recordToTuples(attributes: string[], record:RawRecord, id:RawValue, tuples:RawTuple[] = []):RawTuple[] {
-  throw new Error("@TODO: Implement me!");
+export function recordToTuples(attributes:  string[], record: RawRecord, id: RawValue, tuples: RawTuple[] = []): RawTuple[] {
+  throw new Error("@TODO:  Implement me!");
 }
 
-export function recordToEAVs(record:RawRecord, id:RawValue, eavs:RawChange[] = []):RawChange[] {
-  throw new Error("@TODO: Implement me!");
+export function recordToEAVs(record: RawRecord, id: RawValue, eavs: RawChange[] = []): RawChange[] {
+  throw new Error("@TODO:  Implement me!");
 }
