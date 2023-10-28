a42d4954ca2d90b8101d9c8e3a8edab709959424
cmontella <cmontella@live.com>
2023-10-28 02:00:28 -0400

    Fix notebook

--- src/bin/notebook.rs
+++ src/bin/notebook.rs
@@ -1,135 +1,46 @@
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![allow(warnings)]
-#![recursion_limit="256"]
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::{egui};
-use eframe::egui::{containers::*, *};
-use egui::style::Margin;
-use egui_extras::{StripBuilder, Size};
+use mech_gui::app::*;
+use mech_notebook::*;
+use egui::Vec2;
 
-use mech_gui::button::MyButton;
-use mech_gui::tabs::MyButtonTabs;
-use mech_gui::{FrameStroke,Style};
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-use native_dialog::{FileDialog, MessageDialog, MessageType};
-
-extern crate mech;
-
-use mech::utilities::*;
-use mech::program::*;
-use crate::epaint::Shadow;
-use mech::core::*;
-use mech::syntax::compiler::Compiler;
-use std::thread::JoinHandle;
-extern crate image;
-use std::path::Path;
-use std::collections::{HashMap, HashSet};
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::rc::Rc;
-use std::cell::RefCell;
-use std::sync::Arc;
-use std::sync::Mutex;
-
-use std::io::Cursor;
-use image::io::Reader as ImageReader;
-use image::{ImageBuffer, Pixel, Rgba};
-
-pub fn load_mech_from_path(program_path: &str) -> Result<mech::core::Core,MechError> {
-  match fs::read_to_string(program_path) {
-    Ok(code) => {
-      let mut mcore = mech::core::Core::new();
-      let mut compiler = Compiler::new(); 
-      let core = match compiler.compile_str(&code) {
-        Ok(sections) => {
-          let load_result = mcore.load_sections(sections);
-          for (_,_,errors) in load_result {
-            println!("{:?}", errors);
-          }
-        }
-        Err(x) => {
-          return Err(MechError{msg: "".to_string(), id: 87491, kind: MechErrorKind::GenericError(format!("{:?}",x))})
-        }
-      };
-      let mut code = r#"
-#time/timer = [|period<ms> ticks<u64>|]
-#notebook/compiler = [|code<string>| "hi"]
-#io/pointer = [|x<f32> y<f32>| 0 0]"#.to_string();
-        
-        code += r#"
-#mech/tables = ["time/timer"
-                "io/pointer"
-                "mech/tables"
-                "notebook/compiler""#;
-      for name in mcore.table_names() {
-        code += &format!("\n{:?}",name);     
-      }
-      code += "]";
-      
-      let mut compiler = Compiler::new();
-      let sections = compiler.compile_str(&code).unwrap();
-      let load_result = mcore.load_sections(sections);
-      for (_,_,errors) in load_result {
-        println!("{:?}", errors);
-      }
-      mcore.schedule_blocks()?;
-      Ok(mcore)
-    },
-    Err(err) => Err(MechError{msg: "".to_string(), id: 87491, kind: MechErrorKind::GenericError(format!("{:?}",err))}),
-  }
+    let mut native_options = eframe::NativeOptions::default();
+    let icon = icon::load_icon();
+    let core = load_mech().unwrap();
+    native_options.icon_data = Some(icon);
+    native_options.min_window_size = Some(Vec2{x: 500.0, y: 400.0});
+    native_options.initial_window_size = Some(Vec2{x: 1280.0, y: 720.0});
+    //eframe::run_native("Mech Notebook", native_options, Box::new(|cc| 
+    //  Box::new(MechApp::new(cc,core))));
+    eframe::run_native(
+        "Mech Notebook",
+        native_options,
+        Box::new(|cc| Box::new(MechApp::new(cc,core))),
+    )
 }
 
-pub fn load_mech() -> Result<mech::core::Core,MechError> {
-  let code_string = include_str!(r#"notebook.mec"#);
-  let mut mcore = mech::core::Core::new();
-  let mut compiler = Compiler::new(); 
-  match compiler.compile_str(&code_string) {
-    Ok(sections) => {
-      mcore.load_sections(sections);
-    }
-    Err(x) => {
-      
-    }
-  }
-  
-  let mut code = r#"
-#time/timer = [|period<ms> ticks<u64>|]
-#notebook/compiler = [|code<string>| "hi"]
-#io/pointer = [|x<f32> y<f32> primary-down<bool>| 0 0 ✗]
-#io/keyboard = [|space enter|
-                 ✗     ✗]"#.to_string();
-  code += r#"
-#mech/tables = [|name<string>|
-                "time/timer"
-                "io/pointer"
-                "io/keyboard"
-                "mech/tables"
-                "notebook/compiler""#;
-  for (table,row,col) in &mcore.output {
-    let table = match mcore.dictionary.borrow().get(table.unwrap()) {
-      Some(name) => {code += &format!("\n{:?}",name.to_string());}
-      None => (),
-    };
-  }
-  code += "]";
-  let mut compiler = Compiler::new();
-  let sections = compiler.compile_str(&code).unwrap();
-  mcore.load_sections(sections);
-  mcore.schedule_blocks();
-  Ok(mcore)
-}
-
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
 fn main() {
-  //let input = std::env::args().nth(1).unwrap();
-  /*let mut native_options = eframe::NativeOptions::default();
-  let icon = load_icon();
-  let core = load_mech().unwrap();
-  //native_options.icon_data = Some(icon);
-  native_options.min_window_size = Some(Vec2{x: 500.0, y: 400.0});
-  native_options.initial_window_size = Some(Vec2{x: 1280.0, y: 720.0});
-  eframe::run_native("Mech Notebook", native_options, Box::new(|cc| 
-    Box::new(MechApp::new(cc,core))));*/
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(TemplateApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,94 @@ use eframe::egui::*;
 use eframe::egui::style::{Margin,WidgetVisuals};
 
 pub mod icon;
+
+use mech_utilities::*;
+use mech_program::*;
+use mech_core::*;
+use mech_syntax::compiler::Compiler;
+
+use std::fs;
+
+pub fn load_mech_from_path(program_path: &str) -> Result<mech_core::Core,MechError> {
+  match fs::read_to_string(program_path) {
+    Ok(code) => {
+      let mut mcore = mech_core::Core::new();
+      let mut compiler = Compiler::new(); 
+      let core = match compiler.compile_str(&code) {
+        Ok(sections) => {
+          let load_result = mcore.load_sections(sections);
+          for (_,_,errors) in load_result {
+            println!("{:?}", errors);
+          }
+        }
+        Err(x) => {
+          return Err(MechError{msg: "".to_string(), id: 87491, kind: MechErrorKind::GenericError(format!("{:?}",x))})
+        }
+      };
+      let mut code = r#"
+#time/timer = [|period<ms> ticks<u64>|]
+#notebook/compiler = [|code<string>| "hi"]
+#io/pointer = [|x<f32> y<f32>| 0 0]"#.to_string();
+        
+        code += r#"
+#mech/tables = ["time/timer"
+                "io/pointer"
+                "mech/tables"
+                "notebook/compiler""#;
+      for name in mcore.table_names() {
+        code += &format!("\n{:?}",name);     
+      }
+      code += "]";
+      
+      let mut compiler = Compiler::new();
+      let sections = compiler.compile_str(&code).unwrap();
+      let load_result = mcore.load_sections(sections);
+      for (_,_,errors) in load_result {
+        println!("{:?}", errors);
+      }
+      mcore.schedule_blocks()?;
+      Ok(mcore)
+    },
+    Err(err) => Err(MechError{msg: "".to_string(), id: 87491, kind: MechErrorKind::GenericError(format!("{:?}",err))}),
+  }
+}
+
+pub fn load_mech() -> Result<mech_core::Core,MechError> {
+  let code_string = include_str!(r#"bin/notebook.mec"#);
+  let mut mcore = mech_core::Core::new();
+  let mut compiler = Compiler::new(); 
+  match compiler.compile_str(&code_string) {
+    Ok(sections) => {
+      mcore.load_sections(sections);
+    }
+    Err(x) => {
+      
+    }
+  }
+  
+  let mut code = r#"
+#time/timer = [|period<ms> ticks<u64>|]
+#notebook/compiler = [|code<string>| "hi"]
+#io/pointer = [|x<f32> y<f32> primary-down<bool>| 0 0 ✗]
+#io/keyboard = [|space enter|
+                 ✗     ✗]"#.to_string();
+  code += r#"
+#mech/tables = [|name<string>|
+                "time/timer"
+                "io/pointer"
+                "io/keyboard"
+                "mech/tables"
+                "notebook/compiler""#;
+  for (table,row,col) in &mcore.output {
+    let table = match mcore.dictionary.borrow().get(table.unwrap()) {
+      Some(name) => {code += &format!("\n{:?}",name.to_string());}
+      None => (),
+    };
+  }
+  code += "]";
+  let mut compiler = Compiler::new();
+  let sections = compiler.compile_str(&code).unwrap();
+  mcore.load_sections(sections);
+  mcore.schedule_blocks();
+  Ok(mcore)
+}
